34da190730bb8f2c7f3927801a0b6e338c2a1639
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-17 12:41:22 -0500

    Build the initial parts of the program

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var questions= [{
+    que1: "Commonly used data types do not include: ",
+    ans: "strings\nbooleans\nalerts\nnumbers",
+    answer: "alerts"
+},
+{
+    que2: "The condition in an if/else statement is enclosed with_____.",
+    ans: "quotes\ncurly brackets\nparenthesis\nsquare brackets",
+    answer: "parenthesis"
+},
+{
+    que3: "Arrays in JavaScript can be used to store _____.",
+    ans:"numbers and strings\nother arrays\nbooleans\nall of the above",
+    answer: "all of the above"
+},
+{
+    que4: "String values must be enclosed within _____ when being assigned to variables.",
+    ans: "commmas\ncurly brackets\nquotes\nparenthesis",
+    answer:"quotes"
+},
+{
+    que5: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    ans: "JavaScript\nterminal/bash\nfor loops\nconsole.log",
+    answer: "console.log"
+}
+];
+
+var holdIntro= document.getElementsByClassName("display_content");
+var timer= document.getElementById("time");
+var begin= document.querySelector("#startBtn");
+function startQuiz(){
+    
+}
+
+begin.addEventListener("click", startQuiz);
+function endGame(){
+    
+}
