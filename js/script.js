5e6ecaf11e1cafe4806c30d15e74a5edca1407e0
Obinna Ezekwe <ozekwe@gmail.com>
2020-10-18 09:09:37 -0500

    Printing out question and multiple answers

--- js/script.js
+++ js/script.js
@@ -1,38 +1,62 @@
 var questions= [{
-    que1: "Commonly used data types do not include: ",
-    ans: "strings\nbooleans\nalerts\nnumbers",
+    que: "Commonly used data types do not include: ",
+    choices: ["strings","booleans","alerts","numbers"],
     answer: "alerts"
 },
 {
-    que2: "The condition in an if/else statement is enclosed with_____.",
-    ans: "quotes\ncurly brackets\nparenthesis\nsquare brackets",
+    que: "The condition in an if/else statement is enclosed with_____.",
+    choices: ["quotes","curly brackets","parenthesis","square brackets"],
     answer: "parenthesis"
 },
 {
-    que3: "Arrays in JavaScript can be used to store _____.",
-    ans:"numbers and strings\nother arrays\nbooleans\nall of the above",
+    que: "Arrays in JavaScript can be used to store _____.",
+    choices: ["numbers and strings","other arrays","booleans","all of the above"],
     answer: "all of the above"
 },
 {
-    que4: "String values must be enclosed within _____ when being assigned to variables.",
-    ans: "commmas\ncurly brackets\nquotes\nparenthesis",
+    que: "String values must be enclosed within _____ when being assigned to variables.",
+    choices: ["commmas","curly brackets","quotes","parenthesis"],
     answer:"quotes"
 },
 {
-    que5: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    ans: "JavaScript\nterminal/bash\nfor loops\nconsole.log",
+    que: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript","terminal/bash","for loops","console.log"],
     answer: "console.log"
 }
 ];
 
-var holdIntro= document.getElementsByClassName("display_content");
+var index=0;
+var introPg= document.getElementById("intro");
+var queS= document.getElementById("quizSection");
+var options= document.getElementById("choices");
+var endPg= document.getElementById("conclusion");
 var timer= document.getElementById("time");
 var begin= document.querySelector("#startBtn");
+
+
 function startQuiz(){
-    
+    introPg.setAttribute("class", "Remove");
+    queS.removeAttribute("class");
+    displayQuestion();
+}
+function displayQuestion(){
+    let initialProblem= questions[index];
+    document.getElementById("print_question").innerText= initialProblem.que;
+    options.innerHTML= "";
+    for(let x=0; x<initialProblem.choices.length; x++){
+        let quizNode= document.createElement("button");
+        quizNode.setAttribute("value", initialProblem.choices[x]);
+        quizNode.textContent= `${initialProblem.choices[x]}`;
+        quizNode.onclick= checkAnswer();
+        options.appendChild(quizNode);
+    }
 }
 
-begin.addEventListener("click", startQuiz);
+function checkAnswer(){
+
+}
+
 function endGame(){
     
 }
+begin.addEventListener("click", startQuiz);
